03ffea11860f1d49303eb3c9098a339b9c826db0
Lucas Prins <lucas2002prins@gmail.com>
2022-09-29 21:03:39 +0200

    Tailwind Setup

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <p>
+        <p className='bg-gray-600'>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <span>
