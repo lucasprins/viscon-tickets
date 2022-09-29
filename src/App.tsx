d55675edef96160f3c387ecc8ab272b5ab709418
Lucas Prins <lucas2002prins@gmail.com>
2022-09-29 21:42:11 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <p className='bg-gray-600'>
+        <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <span>
