63d55db61a7c9f23fba6ebb2ab922a7a850b7535
Lucas Prins <lucas2002prins@gmail.com>
2022-10-01 11:20:47 +0200

    Fix: missing border on mobile sidebar

--- src/components/Navigation/NavigationSidebarMobile.tsx
+++ src/components/Navigation/NavigationSidebarMobile.tsx
@@ -9,8 +9,8 @@ export function NavigationSidebarMobile({ state }: NavigationSidebarMobileType) 
 
 	return (
 		<aside className={state
-			? "h-screen w-72 absolute inset-0 z-50 drop-shadow duration-500 transition-all flex md:hidden flex-col justify-between py-8 bg-white dark:border-dark-500 dark:bg-dark-700"
-			: "-translate-x-full h-screen w-72 absolute inset-0 z-50 drop-shadow duration-500 md:hidden transition-all flex flex-col justify-between py-8 bg-white dark:border-dark-500 dark:bg-dark-700"
+			? "h-screen w-72 fixed inset-0 z-50 duration-500 transition-all flex md:hidden flex-col justify-between border-r py-8 bg-white dark:border-dark-500 dark:bg-dark-700"
+			: "-translate-x-full h-screen w-72 fixed inset-0 z-50 duration-500 border-r md:hidden transition-all flex flex-col justify-between py-8 bg-white dark:border-dark-500 dark:bg-dark-700"
 		}>
 			<NavigationSidebar />
 		</aside>
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../../components/Layout/Layout';
 
 export function Dashboard() {
@@ -6,7 +6,11 @@ export function Dashboard() {
 	return (
 		<div className='flex flex-col md:flex-row dark:bg-dark-800 dark:text-white'>
 			<Layout />
-			<h1>Dashboard</h1>
+			<h1 className='h-72'>Dashboard</h1>
+			<h1 className='h-72'>Dashboard</h1>
+			<h1 className='h-72'>Dashboard</h1>
+			<h1 className='h-72'>Dashboard</h1>
+			<h1 className='h-72'>Dashboard</h1>
 		</div>
 	);
 }
