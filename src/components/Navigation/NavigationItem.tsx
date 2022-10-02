d139c6fa9535efe183e33274993be98164169ea7
Lucas Prins <lucas2002prins@gmail.com>
2022-10-02 21:34:58 +0200

    Fix: changed nav item dark mode color

--- src/components/Navigation/NavigationItem.tsx
+++ src/components/Navigation/NavigationItem.tsx
@@ -12,7 +12,7 @@ export function NavigationItem({ name, icon, url }: NavigationItemProps) {
 		<NavLink to={'/' + url} className={({ isActive }) => isActive ? 'bg-gray-100 rounded-md dark:bg-dark-600' : undefined}>
 			<div className='flex items-center px-3 py-2 gap-x-2 hover:bg-gray-100 dark:hover:bg-dark-600 rounded-md'>
 				{icon}
-				<span className='text-md font-semibold text-gray-700 dark:text-dark-300 dark:font-medium'>{name}</span>
+				<span className='text-md font-semibold text-gray-700 dark:text-white dark:font-medium'>{name}</span>
 			</div>
 		</NavLink>
 	);
