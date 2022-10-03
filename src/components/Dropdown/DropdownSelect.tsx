791c2b0b3c75efb5b34569e0061e6f4fd1bfc90b
Lucas Prins <lucas2002prins@gmail.com>
2022-10-03 10:40:35 +0200

    Fix: Dropdown Outline color en width

--- src/components/Dropdown/DropdownSelect.tsx
+++ src/components/Dropdown/DropdownSelect.tsx
@@ -18,7 +18,7 @@ export function DropdownSelectMachine({ machines, label }: DropdownSelectProps) 
             <div className='mb-1.5'>
                 <Listbox.Label className='text-sm font-medium text-gray-700 dark:text-dark-300'>{label}</Listbox.Label>
             </div>
-            <Listbox.Button className='w-full rounded-lg mb-2 flex border bg-white text-gray-800 font-medium shadow-sm border-gray-200 py-2.5 px-3.5 dark:text-white dark:font-normal dark:border-dark-500 dark:bg-dark-700'>
+            <Listbox.Button className=' focus:outline-4 dark:outline-0 outline-primary-200 w-full rounded-lg mb-2 flex border bg-white text-gray-800 font-medium shadow-sm border-gray-200 py-2.5 px-3.5 dark:text-white dark:font-normal dark:border-dark-500 dark:bg-dark-700'>
                 {({ open }) => (
                     <div className='flex justify-between w-full'>
                         {selectedMachine.name}
@@ -26,7 +26,7 @@ export function DropdownSelectMachine({ machines, label }: DropdownSelectProps) 
                     </div>
                 )}
             </Listbox.Button>
-            <Listbox.Options className='z-index cursor-pointer 50 overflow-y-scroll p-1.5 h-64 rounded-lg shadow dark:bg-dark-700 text-gray-800 font-medium flex flex-col gap-1 border border-gray-200 dark:text-white dark:font-normal dark:border-dark-500'>
+            <Listbox.Options className='focus:outline-4 outline-primary-200 dark:outline-0 z-index cursor-pointer 50 overflow-y-scroll p-1.5 h-64 rounded-lg shadow dark:bg-dark-700 text-gray-800 font-medium flex flex-col gap-1 border border-gray-200 dark:text-white dark:font-normal dark:border-dark-500'>
                 {machines.map((machine) => (
                     <Listbox.Option key={machine.machine_id} value={machine}>
                         {({ active, selected }) => (
