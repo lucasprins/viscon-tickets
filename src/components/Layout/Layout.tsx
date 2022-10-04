39c72925e8851dc57a38a0917dc3a6693d372652
Lucas Prins <lucas2002prins@gmail.com>
2022-10-04 11:24:20 +0200

    Fix

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react'
-import Backdrop from '../Backdrop/Backdrop'
-import { NavigationMobile } from '../Navigation/NavigationMobile'
-import { NavigationSidebarDesktop } from '../Navigation/NavigationSidebarDesktop'
-import { NavigationSidebarMobile } from '../Navigation/NavigationSidebarMobile'
+import React, { useState } from "react";
+import Backdrop from "../Backdrop/Backdrop";
+import { NavigationMobile } from "../Navigation/NavigationMobile";
+import { NavigationSidebarDesktop } from "../Navigation/NavigationSidebarDesktop";
+import { NavigationSidebarMobile } from "../Navigation/NavigationSidebarMobile";
 
 export default function Layout() {
-    const [sidebar, setSidebar] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
 
-    const toggleSidebar = () => {
-        setSidebar((prevState) => !prevState);
-    };
+  const toggleSidebar = () => {
+    setSidebar((prevState) => !prevState);
+  };
 
-    return (
-        <>
-            <NavigationMobile openSidebar={toggleSidebar} />
-            <Backdrop state={sidebar} close={toggleSidebar} />
-            <NavigationSidebarMobile state={sidebar} />
-            <NavigationSidebarDesktop />
-        </>
-    )
+  return (
+    <>
+      <NavigationMobile openSidebar={toggleSidebar} />
+      <Backdrop state={sidebar} close={toggleSidebar} />
+      <NavigationSidebarMobile state={sidebar} />
+      <NavigationSidebarDesktop />
+    </>
+  );
 }
