31032bb5a93c0a06d0ac8495c5024626acd98e3a
Lucas Prins <lucas2002prins@gmail.com>
2022-10-07 12:17:10 +0200

    Added: Icons

--- src/pages/Playground/Playground.tsx
+++ src/pages/Playground/Playground.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Breadcrumbs } from '../../components/Breadcrumbs/Breadcrumbs';
 import { Button, ButtonSize, ButtonType, ButtonWidth } from '../../components/Button/Button';
 import { ButtonLink, ButtonLinkSize, ButtonLinkType } from '../../components/Button/ButtonLink';
+import { IconAlert } from '../../components/Icons/IconAlert';
+import { IconImage } from '../../components/Icons/IconImage';
+import { IconPhone } from '../../components/Icons/IconPhone';
 import { IconTicket } from '../../components/Icons/IconTicket';
+import { IconTrash } from '../../components/Icons/IconTrash';
+import { IconUpload } from '../../components/Icons/IconUpload';
 import Layout from '../../components/Layout/Layout';
 
 export function Playground() {
@@ -13,24 +18,7 @@ export function Playground() {
 			<div className='p-8 flex w-full  flex-col gap-y-12'>
 				<Breadcrumbs crumbs={['Playground']} />
 				<div className='w-72 flex gap-2'>
-					{/* <ButtonLink
-						size={ButtonLinkSize.MEDIUM}
-						type={ButtonLinkType.COLOR}
-						text="Create a ticket"
-						url="/knowledgebase/create-ticket"
-					/> */}
-					<Button
-						size={ButtonSize.MEDIUM}
-						type={ButtonType.TERTIARY_COLOR}
-						text="No"
-						width={ButtonWidth.FULL}
-					/>
-					<Button
-						size={ButtonSize.MEDIUM}
-						type={ButtonType.PRIMARY}
-						text="Yes"
-						width={ButtonWidth.FULL}
-					/>
+					<IconTrash size='24' color='stroke-primary-500 dark:stroke-gray-300' fill='fill-primary-500' />
 				</div>
 			</div>
 		</div>
