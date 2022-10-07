97fbec373670324aeb7fadba859773ab822c34e2
Lucas Prins <lucas2002prins@gmail.com>
2022-10-07 13:21:49 +0200

    Added: IconVideo component

--- src/pages/Playground/Playground.tsx
+++ src/pages/Playground/Playground.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import { Breadcrumbs } from '../../components/Breadcrumbs/Breadcrumbs';
-import { Button, ButtonSize, ButtonType, ButtonWidth } from '../../components/Button/Button';
-import { ButtonLink, ButtonLinkSize, ButtonLinkType } from '../../components/Button/ButtonLink';
 import { FeaturedIcon, FeaturedIconSize, FeaturedIconType } from '../../components/FeaturedIcon/FeaturedIcon';
 import { IconAlert } from '../../components/Icons/IconAlert';
-import { IconImage } from '../../components/Icons/IconImage';
-import { IconPhone } from '../../components/Icons/IconPhone';
-import { IconTicket } from '../../components/Icons/IconTicket';
-import { IconTrash } from '../../components/Icons/IconTrash';
-import { IconUpload } from '../../components/Icons/IconUpload';
+import { IconVideo } from '../../components/Icons/IconVideo';
 import Layout from '../../components/Layout/Layout';
 
 export function Playground() {
@@ -19,7 +13,7 @@ export function Playground() {
 			<div className='p-8 flex w-full  flex-col gap-y-12'>
 				<Breadcrumbs crumbs={['Playground']} />
 				<div className='w-72 flex gap-2'>
-					<IconTrash size='24' color='stroke-primary-500 dark:stroke-gray-300' fill='fill-primary-500' />
+					<IconVideo size='24' color='stroke-primary-500 dark:stroke-gray-300' fill='fill-primary-500' />
 				</div>
 				<div className='w-72 flex gap-2 items-center'>
 					<FeaturedIcon type={FeaturedIconType.PRIMARY} size={FeaturedIconSize.SM} icon={<IconAlert size='15' color='stroke-primary-500 dark:stroke-gray-300' fill='fill-primary-500' />} />
