388438a2c7a2ba7f1063977112ff049fc4fcd925
9iiota <crazyshocklight@gmail.com>
2022-10-16 18:54:31 +0200

    a

---
+++ client/src/components/atoms/Input/InputCheckbox.tsx
@@ -0,0 +1,9 @@
+import { Field } from "formik";
+import React, { useState } from "react";
+
+export function InputCheckbox() {
+  return (
+    // <div className=""></div>
+    <input type="checkbox"></input>
+  )
+}
--- client/src/components/pages/Playground/Playground.tsx
+++ client/src/components/pages/Playground/Playground.tsx
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Badge } from "../../atoms/Badge/Badge";
 import { Breadcrumbs } from "../../atoms/Breadcrumbs/Breadcrumbs";
-import { IconAlert } from "../../atoms/Icons/Icons";
+import { IconAlert, IconMenu } from "../../atoms/Icons/Icons";
+import { InputCheckbox } from "../../atoms/Input/InputCheckbox";
+import { InputField } from "../../atoms/Input/InputField";
+import { InputLabel } from "../../atoms/Input/InputLabel";
+import { PageHeader } from "../../atoms/PageHeader/PageHeader";
 import Layout from "../../organisms/Layout/Layout";
+import { NavigationHeader } from "../../organisms/Navigation/NavigationHeader";
 import { BasicTable } from "../../organisms/Table/basicTable";
 
 export function Playground() {
+    const [email, setEmail] = useState({
+        email2: ""
+    });
+
     return (
         <div className='flex flex-col md:flex-row dark:bg-dark-800 dark:text-white'>
             <Layout />
             <div className='p-8 flex flex-col gap-y-5'>
-                <Breadcrumbs crumbs={["Playground"]} />
+                <NavigationHeader />
 
-                <Badge
-                    size='lg'
-                    color='error'
-                    text='Primary'
-                    icon={<IconAlert size='14' fill='fill-error-500' color='stroke-error-500' />}
-                />
+                <div className="px-8">
+                    <PageHeader title="Log in" subtitle="Welcome back! Please enter your details." />
 
-                <BasicTable />
+                    <InputLabel htmlFor="fsd" text="Email"/>
+                    {/* <InputField
+                        style='icon'
+                        type='email'
+                        placeholder="Enter your email"
+                        icon={
+                            <IconMenu
+                                size='20'
+                                color='stroke-gray-500'
+                                fill='fill-primary-500'
+                            />
+                        }
+                        id='email2'
+                        name='email2'
+                    /> */}
+
+                    {/* <InputCheckbox /> */}
+
+                </div>
+
             </div>
         </div>
     );
