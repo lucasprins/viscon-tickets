bce084ed2bec7cf1e146ccca3a34c3cb5d289d12
Lucas Prins <lucas2002prins@gmail.com>
2022-10-16 18:59:12 +0200

    Update Playground.tsx

--- client/src/components/pages/Playground/Playground.tsx
+++ client/src/components/pages/Playground/Playground.tsx
@@ -1,6 +1,8 @@
+import { Formik, Form } from "formik";
 import React, { useState } from "react";
 import { Badge } from "../../atoms/Badge/Badge";
 import { Breadcrumbs } from "../../atoms/Breadcrumbs/Breadcrumbs";
+import { Button } from "../../atoms/Button/Button";
 import { IconAlert, IconMenu } from "../../atoms/Icons/Icons";
 import { InputCheckbox } from "../../atoms/Input/InputCheckbox";
 import { InputField } from "../../atoms/Input/InputField";
@@ -12,7 +14,12 @@ import { BasicTable } from "../../organisms/Table/basicTable";
 
 export function Playground() {
     const [email, setEmail] = useState({
-        email2: ""
+        email2: "",
+    });
+
+    const loginState = useState({
+        email: "",
+        password: "",
     });
 
     return (
@@ -21,29 +28,36 @@ export function Playground() {
             <div className='p-8 flex flex-col gap-y-5'>
                 <NavigationHeader />
 
-                <div className="px-8">
-                    <PageHeader title="Log in" subtitle="Welcome back! Please enter your details." />
+                <div className='px-8'>
+                    <PageHeader title='Log in' subtitle='Welcome back! Please enter your details.' />
 
-                    <InputLabel htmlFor="fsd" text="Email"/>
-                    {/* <InputField
-                        style='icon'
-                        type='email'
-                        placeholder="Enter your email"
-                        icon={
-                            <IconMenu
-                                size='20'
-                                color='stroke-gray-500'
-                                fill='fill-primary-500'
-                            />
-                        }
-                        id='email2'
-                        name='email2'
-                    /> */}
-
-                    {/* <InputCheckbox /> */}
-
+                    <Formik initialValues={loginState} onSubmit={() => console.log("Submitting login")}>
+                        {({ errors, touched, isValidating }) => (
+                            <Form className='flex flex-col gap-5 w-full'>
+                                <div className='flex gap-4 w-full'>
+                                    <div className='flex flex-col w-full gap-1.5'>
+                                        <InputLabel htmlFor='firstName' text='test' />
+                                        <InputField
+                                            style='iconless'
+                                            type='text'
+                                            id='firstName'
+                                            name='firstName'
+                                            disabled={true}
+                                        />
+                                    </div>
+                                </div>
+                                <Button
+                                    formType='submit'
+                                    size='medium'
+                                    width='full'
+                                    type='primary'
+                                    text='Clicked on login'
+                                    onclick={() => console.log("Clicked on Login")}
+                                />
+                            </Form>
+                        )}
+                    </Formik>
                 </div>
-
             </div>
         </div>
     );
