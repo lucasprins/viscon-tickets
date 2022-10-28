734d23de69868532d609e4e6bf52c433fcf74f2b
Lucas Prins <lucas2002prins@gmail.com>
2022-10-28 15:22:37 +0200

    Add: table

--- client/src/components/pages/Playground/Playground.tsx
+++ client/src/components/pages/Playground/Playground.tsx
@@ -1,7 +1,17 @@
+import { Tab } from '@headlessui/react'
 import React from 'react'
+import { Breadcrumbs } from '../../atoms/Breadcrumbs/Breadcrumbs'
+import Layout from '../../organisms/Layout/Layout'
+import { TableTickets } from '../../organisms/Table/TableTickets'
 
 export const Playground = () => {
   return (
-    <div>Playground</div>
+    <div className='flex flex-col h-screen md:flex-row dark:bg-dark-800 dark:text-white'>
+			<Layout />
+			<div className='p-8'>
+				<Breadcrumbs crumbs={['Playground']}/>
+        <TableTickets />
+			</div>
+		</div>
   )
 }
