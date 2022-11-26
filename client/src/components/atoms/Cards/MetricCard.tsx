7e57a54a763680a698ec43bbf43b7357c0294e49
Lucas Prins <lucas2002prins@gmail.com>
2022-11-26 16:55:12 +0100

    Typefix

--- client/src/components/atoms/Cards/MetricCard.tsx
+++ client/src/components/atoms/Cards/MetricCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const MetricCard = ({ content, title }: { content: any, title: string }) => {
+export const MetricCard = ({ content, title }: { content: string | JSX.Element | number, title: string }) => {
   return (
     <div className='flex flex-col w-full gap-1 p-6 bg-white border border-gray-200 rounded-xl dark:border-dark-600 dark:bg-dark-700 drop-shadow-sm'>
         <span className='text-sm font-medium text-gray-600 dark:text-dark-300'>{title}</span>
