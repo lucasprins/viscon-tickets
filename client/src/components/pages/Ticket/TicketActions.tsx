ab118cc9b0c407f4c25c69c4b544061d7f77a0c3
Lucas Prins <lucas2002prins@gmail.com>
2022-12-12 18:29:54 +0100

    Update TicketActions.tsx

--- client/src/components/pages/Ticket/TicketActions.tsx
+++ client/src/components/pages/Ticket/TicketActions.tsx
@@ -181,7 +181,7 @@ export const TicketActions = ({
             width='content'
             type='primary'
             text={translations[language].resolve}
-            disabled={unclaimingTicket || resolvingTicket || (ticket.solution != "" )}
+            disabled={unclaimingTicket || resolvingTicket || (ticket.solution == "" )}
             onclick={resolveTicket}
             icon={
               resolvingTicket ? (
@@ -199,7 +199,7 @@ export const TicketActions = ({
             width='full'
             type='primary'
             text={translations[language].resolve}
-            disabled={unclaimingTicket || resolvingTicket || (ticket.solution != "" )}
+            disabled={unclaimingTicket || resolvingTicket || (ticket.solution != " " && ticket.solution != null )}
             onclick={resolveTicket}
             icon={
               resolvingTicket ? (
