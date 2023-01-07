667234049e2d323eb1f3b2e0ffb77bf4358b48c2
Lucas Prins <lucas2002prins@gmail.com>
2023-01-07 11:41:12 +0100

    Updated README.MD and .env files

--- client/src/features/user/userService.ts
+++ client/src/features/user/userService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { authHeader } from "../auth/authHeader";
 
-const API_URL = "https://localhost:7295/api/user/";
+const API_URL = `${process.env.REACT_APP_API_URL}user/`;
 
 const getUsers = async () => {
   const response = await axios.get(API_URL + "GetUsers", {
