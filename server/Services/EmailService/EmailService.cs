8e4d7cb96647b0ebcd402c69442e418372514520
Lucas Prins <lucas2002prins@gmail.com>
2023-01-10 19:26:41 +0100

    update

--- server/Services/EmailService/EmailService.cs
+++ server/Services/EmailService/EmailService.cs
@@ -11,40 +11,47 @@ namespace server.Services.EmailService
   {
 
     private readonly IConfiguration _configuration;
+    private readonly DataContext _context;
 
-    public EmailService(IConfiguration configuration)
+    public EmailService(IConfiguration configuration, DataContext dataContext)
     {
       _configuration = configuration;
+      _context = dataContext;
     }
 
-        public async Task SendNewTicketEmail(string email)
-        {
-          System.Console.WriteLine("Sending email to " + email);
+    public async Task SendNewTicketEmail(Ticket ticket)
+    {
+      var visconEmployees = await _context.Users.Where(u => u.Company.Name.Contains("Viscon")).ToListAsync();
+      var company = await _context.Companies.FindAsync(ticket.CompanyId);
 
-          var mail = new MimeMessage();
-          mail.From.Add(MailboxAddress.Parse(_configuration.GetSection("EmailAddress").Value));
-          mail.To.Add(MailboxAddress.Parse(email));
-          mail.Subject = "New Viscon Ticket";
-          mail.Body = new TextPart("plain")
-          {
-            Text = "A new ticket has been created. Please log in to view it."
-          };
+      var mail = new MimeMessage();
+      mail.From.Add(MailboxAddress.Parse(_configuration.GetSection("EmailAddress").Value));
+      
+      foreach(var employee in visconEmployees) {
+        mail.To.Add(MailboxAddress.Parse(employee.Email));
+      }
 
-          using (var client = new SmtpClient())
-          {
-            await client.ConnectAsync(
-                _configuration.GetSection("EmailHost").Value,
-                int.TryParse(_configuration.GetSection("EmailPort").Value, System.Globalization.NumberStyles.Number, System.Globalization.CultureInfo.InvariantCulture, out int port) ? port : 587,
-                MailKit.Security.SecureSocketOptions.StartTls);
+      mail.Subject = $"New Ticket: {ticket.TicketNumber} by {company?.Name}";
+      mail.Body = new TextPart("plain")
+      {
+        Text = $"A new ticket has been created. You can view it at http://localhost:3000/tickets/{ticket.Id}"
+      };
 
-            await client.AuthenticateAsync(_configuration.GetSection("EmailAddress").Value, _configuration.GetSection("EmailPassword").Value);
+      using (var client = new SmtpClient())
+      {
+        await client.ConnectAsync(
+            _configuration.GetSection("EmailHost").Value,
+            int.TryParse(_configuration.GetSection("EmailPort").Value, System.Globalization.NumberStyles.Number, System.Globalization.CultureInfo.InvariantCulture, out int port) ? port : 587,
+            MailKit.Security.SecureSocketOptions.StartTls);
 
-            await client.SendAsync(mail);
-            await client.DisconnectAsync(true);
-          }
-        }
+        await client.AuthenticateAsync(_configuration.GetSection("EmailAddress").Value, _configuration.GetSection("EmailPassword").Value);
 
-        public async Task SendRegisterEmail(string email, string token)
+        await client.SendAsync(mail);
+        await client.DisconnectAsync(true);
+      }
+    }
+
+    public async Task SendRegisterEmail(string email, string token)
     {
       System.Console.WriteLine("Sending email to " + email);
 
